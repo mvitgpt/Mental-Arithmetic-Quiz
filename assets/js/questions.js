1b3873d8f9ef1164ca39c07ccbc85ef7322da040
Rashidat Adekoya <rawsheedah@yahoo.com>
2022-10-18 17:58:16 +0000

    Add questions with level content to questions file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -20,7 +20,7 @@ let questionsLevel1 = [
 
 {
     num: 4,
-    question: 3 + "?" = 12,
+    question: "3 + ? = 12",
     answer: 9
 },
 
@@ -32,7 +32,7 @@ let questionsLevel1 = [
 
 {
     num: 6,
-    question: Increase 17 by 8,
+    question: "Increase 17 by 8",
     answer: 25
 },
 
@@ -56,69 +56,133 @@ let questionsLevel1 = [
 
 {
     num: 10,
-    question: 6 x 2 = 3 x "",
+    question: "6 x 2 = 3 x ?",
     answer: 4
 },
 ] 
 
+// Level 2 Questions
 let questionsLevel2 = [
     {
     num: 1,
-    question: 7 x 4 = 30 - "",
+    question: "7 x 4 = 30 - ?",
     answer: 2
 },
 
 {
     num: 2,
-    question: ,
-    answer: 
+    question: "What is one quarter of 20 ?",
+    answer: 5
 },
    
 {
     num: 3,
-    question: ,
-    answer: 
-},
-
-{
-    num: 4,
-    question: 3 + "?" = 12,
-    answer: 9
-},
-
-{
-    num: 5,
-    question: ,
-    answer: 
-},
-
-{
-    num: 6,
-    question: Increase 17 by 8,
+    question: "Find the sum of one 10p and three 5p",
     answer: 25
 },
 
 {
+    num: 4,
+    question: "? + 7 = 15",
+    answer: 8
+},
+
+{
+    num: 5,
+    question:" 8 / 2 = 16 / ?",
+    answer: 4
+},
+
+{
+    num: 6,
+    question: "What is one-fifth of thirty ?",
+    answer: 6
+},
+
+{
     num: 7,
-    question: ,
-    answer: 
+    question: "3 x 9 = 9 + 9 + ?",
+    answer: 9
 },
 
 {
     num: 8,
-    question: ,
-    answer: 
+    question: "How much less is seven 2ps than 20p ?",
+    answer: 6
 },
 
 {
     num: 9,
     question: "What is half an hour minus ten minutes ?",
-    answer: 20 minutes
+    answer: 20 
 },
 
 {
     num: 10,
     question: 44 - 9,
     answer: 35
+},
+]
+
+// Level 3 Questions
+let questionsLevel3 = [
+    {
+    num: 1,
+    question: "Subtract the product of 6 and 7 from 50",
+    answer: 8
+},
+
+{
+    num: 2,
+    question: "A metre is divided into 10 equal parts. How long is each part ?",
+    answer: "10 cm"
+},
+   
+{
+    num: 3,
+    question: 820 - 40,
+    answer: 780
+},
+
+{
+    num: 4,
+    question: 1009 - 100,
+    answer: 909
+},
+
+{
+    num: 5,
+    question: "From the sum of 39 and 6, Subtract 20",
+    answer: 25
+},
+
+{
+    num: 6,
+    question: "From 5 take away 3.08 ?",
+    answer: 1.92
+},
+
+{
+    num: 7,
+    question: "What is 378cm to the nearest metre ?",
+    answer: "4m"
+},
+
+{
+    num: 8,
+    question: "Divide 1.50 by 6 ?",
+    answer: 0.25
+},
+
+{
+    num: 9,
+    question: "7 x 9 = 50 + ?",
+    answer: 13 
+},
+
+{
+    num: 10,
+    question: "99 plus 99 plus 99",
+    answer: 297
 },
 ]
