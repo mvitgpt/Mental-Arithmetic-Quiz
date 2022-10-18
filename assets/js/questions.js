477dcbf47d203cec68e57b311c7e420922a58e9f
Rashidat Adekoya <rawsheedah@yahoo.com>
2022-10-18 16:26:33 +0000

    Add questions file to js file

---
+++ assets/js/questions.js
@@ -0,0 +1,124 @@
+//Array of question number, questions and answers of different levels
+let questionsLevel1 = [
+    {
+    num: 1,
+    question: 61 + 4,
+    answer: 65
+},
+
+{
+    num: 2,
+    question: 15 - 8,
+    answer: 7
+},
+   
+{
+    num: 3,
+    question: 6 + 4 + 8,
+    answer: 18
+},
+
+{
+    num: 4,
+    question: 3 + "?" = 12,
+    answer: 9
+},
+
+{
+    num: 5,
+    question: 5 + 4 + 9,
+    answer: 18
+},
+
+{
+    num: 6,
+    question: Increase 17 by 8,
+    answer: 25
+},
+
+{
+    num: 7,
+    question: "How many days are there in three weeks",
+    answer: 21
+},
+
+{
+    num: 8,
+    question: 16 - 7,
+    answer: 9
+},
+
+{
+    num: 9,
+    question: "What is the sixth letter of the alphabets ?",
+    answer: "F"
+},
+
+{
+    num: 10,
+    question: 6 x 2 = 3 x "",
+    answer: 4
+},
+] 
+
+let questionsLevel2 = [
+    {
+    num: 1,
+    question: 7 x 4 = 30 - "",
+    answer: 2
+},
+
+{
+    num: 2,
+    question: ,
+    answer: 
+},
+   
+{
+    num: 3,
+    question: ,
+    answer: 
+},
+
+{
+    num: 4,
+    question: 3 + "?" = 12,
+    answer: 9
+},
+
+{
+    num: 5,
+    question: ,
+    answer: 
+},
+
+{
+    num: 6,
+    question: Increase 17 by 8,
+    answer: 25
+},
+
+{
+    num: 7,
+    question: ,
+    answer: 
+},
+
+{
+    num: 8,
+    question: ,
+    answer: 
+},
+
+{
+    num: 9,
+    question: "What is half an hour minus ten minutes ?",
+    answer: 20 minutes
+},
+
+{
+    num: 10,
+    question: 44 - 9,
+    answer: 35
+},
+]
