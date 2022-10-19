03b9d62738830b50c0ccf214d8237fe3c7eb089b
Rashidat Adekoya <rawsheedah@yahoo.com>
2022-10-19 13:38:24 +0000

    Add difficulty level code to questions.js file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -3,61 +3,71 @@ let questionsLevel1 = [
     {
     num: 1,
     question: 61 + 4,
-    answer: 65
+    answer: 65,
+    difficulty: "easy"
 },
 
 {
     num: 2,
     question: 15 - 8,
-    answer: 7
+    answer: 7,
+    difficulty: "easy"
 },
    
 {
     num: 3,
     question: 6 + 4 + 8,
-    answer: 18
+    answer: 18,
+    difficulty: "easy"
 },
 
 {
     num: 4,
     question: "3 + ? = 12",
-    answer: 9
+    answer: 9,
+    difficulty: "easy"
 },
 
 {
     num: 5,
     question: 5 + 4 + 9,
-    answer: 18
+    answer: 18,
+    difficulty: "easy"
 },
 
 {
     num: 6,
     question: "Increase 17 by 8",
-    answer: 25
+    answer: 25,
+    difficulty: "easy"
 },
 
 {
     num: 7,
     question: "How many days are there in three weeks",
-    answer: 21
+    answer: 21,
+    difficulty: "easy"
 },
 
 {
     num: 8,
     question: 16 - 7,
-    answer: 9
+    answer: 9,
+    difficulty: "easy"
 },
 
 {
     num: 9,
     question: "What is the sixth letter of the alphabets ?",
-    answer: "F"
+    answer: "F",
+    difficulty: "easy"
 },
 
 {
     num: 10,
     question: "6 x 2 = 3 x ?",
-    answer: 4
+    answer: 4,
+    difficulty: "easy"
 },
 ] 
 
@@ -66,61 +76,71 @@ let questionsLevel2 = [
     {
     num: 1,
     question: "7 x 4 = 30 - ?",
-    answer: 2
+    answer: 2,
+    difficulty: "medium"
 },
 
 {
     num: 2,
     question: "What is one quarter of 20 ?",
-    answer: 5
+    answer: 5,
+    difficulty: "medium"
 },
    
 {
     num: 3,
     question: "Find the sum of one 10p and three 5p",
-    answer: 25
+    answer: 25,
+    difficulty: "medium"
 },
 
 {
     num: 4,
     question: "? + 7 = 15",
-    answer: 8
+    answer: 8,
+    difficulty: "medium"
 },
 
 {
     num: 5,
     question:" 8 / 2 = 16 / ?",
-    answer: 4
+    answer: 4,
+    difficulty: "medium"
 },
 
 {
     num: 6,
     question: "What is one-fifth of thirty ?",
-    answer: 6
+    answer: 6,
+    difficulty: "medium"
 },
 
 {
     num: 7,
     question: "3 x 9 = 9 + 9 + ?",
-    answer: 9
+    answer: 9,
+    difficulty: "medium"
 },
 
 {
     num: 8,
     question: "How much less is seven 2ps than 20p ?",
-    answer: 6
+    answer: 6,
+    difficulty: "medium"
 },
 
 {
     num: 9,
     question: "What is half an hour minus ten minutes ?",
-    answer: 20 
+    answer: 20,
+    difficulty: "medium" 
 },
 
 {
     num: 10,
     question: 44 - 9,
-    answer: 35
+    answer: 35,
+    difficulty: "medium"
 },
 ]
 
@@ -129,60 +149,74 @@ let questionsLevel3 = [
     {
     num: 1,
     question: "Subtract the product of 6 and 7 from 50",
-    answer: 8
+    answer: 8,
+    difficulty: "hard"
 },
 
 {
     num: 2,
     question: "A metre is divided into 10 equal parts. How long is each part ?",
-    answer: "10 cm"
+    answer: "10 cm",
+    difficulty: "hard"
 },
    
 {
     num: 3,
     question: 820 - 40,
-    answer: 780
+    answer: 780,
+    difficulty: "hard"
 },
 
 {
     num: 4,
     question: 1009 - 100,
-    answer: 909
+    answer: 909,
+    difficulty: "hard"
 },
 
 {
     num: 5,
     question: "From the sum of 39 and 6, Subtract 20",
-    answer: 25
+    answer: 25,
+    difficulty: "hard"
 },
 
 {
     num: 6,
     question: "From 5 take away 3.08 ?",
-    answer: 1.92
+    answer: 1.92,
+    difficulty: "hard"
 },
 
 {
     num: 7,
     question: "What is 378cm to the nearest metre ?",
-    answer: "4m"
+    answer: 4,
+    difficulty: "hard"
 },
 
 {
     num: 8,
     question: "Divide 1.50 by 6 ?",
-    answer: 0.25
+    answer: 0.25,
+    difficulty: "hard"
 },
 
 {
     num: 9,
     question: "7 x 9 = 50 + ?",
-    answer: 13 
+    answer: 13,
+    difficulty: "hard" 
 },
 
 {
     num: 10,
     question: "99 plus 99 plus 99",
-    answer: 297
+    answer: 297,
+    difficulty: "hard"
 },
 ]
+
+let selectedLevel = document.getElementById("diffulty");
+
+let filteredQuestionsByDifficulty = questions.filter(question => question.difficulty === selectedLevel);
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,52 @@
+/* The following code was taken from the Code Institute course content.*/
+/* Script to run before loading page content */
+
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     let buttons = document.getElementsByTagName("button");
+//     for (let button of buttons) {
+//         button.addEventListener("click", function() {
+//             if (this.getAttribute("data-type") === "submit") {
+//                 alert("You clicked Submit!");
+//             } else {
+//                 let gameType = this.getAttribute("data-type");
+//                 alert(`You clicked ${gameType}`);
+//             }
+//         });
+//     }
+// });
+
+
+
 // Get the home page box
-let box = document.getElementsByClassName('home-page');
+//let box = document.getElementsByClassName('home-page');
+
+// let userrname = document.getElementById("user-name-input")
+// const nextBtn = document.getElementById("next-btn")
+// let usernameText = document.getElementById("user-name")
+
+// nextBtn.addEventListener("click", function(){
+//     console.log(userrname.value, "dhjdhvfhve")
+//     usernameText.innerText = userrname.value
+// }
+// )
+
+
+// let userName = document.getElementById('user-input-area');
+// userName.addEventListener('submit', userInput);
+
+function userInput(event){
+  event.preventDefault();
+  console.log('submitting ...');
+  
+  let enterName = enterName.input['username'].value;
+  let onInput = document.getElementById('selection-area');
+  // onInput = selection-area.innerHTML
+  onInput.style.display ="block";
+
+  userName.submit();
+
+  console.log("userName submitted");
+}
+
