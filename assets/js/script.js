d4c928445dae1c1abda65173437641fcc836daf4
Rashidat Adekoya <rawsheedah@yahoo.com>
2022-10-23 00:01:12 +0000

    Ade code to hide element from display

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,15 +48,41 @@
 
 
 // Get the Elements by their Tag Name
-const userName = document.getElementById("user-name-input");
-const nextBtn = document.getElementsByClassName("next-btn");
-const questionsArea = document.getElementById("question-box-area");
+let userName = document.getElementById("user-name-input")
+const nextBtn = document.getElementById("next-btn")
+const questionsArea = document.getElementById("question-box-area")
+const selection = document.getElementById("selection-area")
+const startBtn = document.getElementsByClassName('start-quiz-btn')
+const ruleBox = document.getElementById('rule-box')
 
-nextBtn.addEventListener("click", runQuiz);
 
-function runQuiz() {
+nextBtn.addEventListener("click", enterName)
+
+
+
+function enterName() {
     console.log("Started");
-    userName.innerText = userName.value;
+    userName.innerText = userName.value
+    selection.innerHTML = userName.value;
+    questionsArea.innerText = userName.value;
+    
+    nextBtn.addEventListener.classList.add('hide')
+   
+    startBtn.classList.add('hide')
+    // ruleBox.addEventListener.classList.add('hide')
+    
+    selectLevel.classList.remove('hide')
+
 }
 
+// startBtn.addEventListener("click", startQuiz)
 
+// function startQuiz(){
+//     startBtn.classList.add('hide')
+//     // ruleBox.addEventListener.classList.add('hide')
+    
+//     selectLevel.classList.remove('hide')
+
+//     startBtn.addEventListener("click", startQuiz)
+// }
+// console.log("startBtn");
