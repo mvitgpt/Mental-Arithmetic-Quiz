16b8cb26e0e9758cf06e44cb78c0b848c1c4d80b
Rashidat Adekoya <rawsheedah@yahoo.com>
2022-10-29 10:31:23 +0000

    Add footer and social media links to index page

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ const backHome = document.getElementById("back-home-btn");
 
 // Select Level of Difficulty Section
 const selection = document.getElementById("selection-area");
-const selectedLevel = document.getElementsByClassName("difficulty");
+const selectLevel = document.getElementsByClassName("difficulty");
 const level1 = document.getElementById("difficulty-level1");
 const level2 = document.getElementById("difficulty-level2");
 const level3 = document.getElementById("difficulty-level3");
@@ -90,10 +90,10 @@ if (errorTxt !== '') {
     selection.style.display = "block"; //show difficulty level box
  });
 
- // Redirect user to question page for each level when home button is clicked
+ // Redirect user to question page for each level when a level is selected 
     level1.addEventListener("click", function(){
-    selection.style.display = "none";
-    questionBox.style.display = "block"; //show question box
+    selection.style.display = "none";      //hide level selection box
+    questionBox.style.display = "block";   //show question box
  });
 
  level2.addEventListener("click", function(){
@@ -118,9 +118,8 @@ if (errorTxt !== '') {
     startQuiz.style.display = "block";
     });
 
-   
 
-
+    
     let timer = 0;
     let interval = 0; 
     let index = 0;
@@ -139,88 +138,185 @@ if (errorTxt !== '') {
 
     // Set variable to Index and empty Array  
     let questionsCounterIndex = 0;
+    let questionNum = 1;
     let currentQuestion;
     let selectedQuestions = [];
     let quiz;
 
-    // Function that loops through questions then Push questions into the selectedQuestions Array
-    function availableSelections() {
-        // const totalQuestions = quiz.length
-        for(let i = 0; i< quiz.length; i++) {
-            selectedQuestions.push(quiz[i]) 
-        }
-        console.log(selectedQuestions);
+    let shuffledQuestions;
+
+    function displayQuestionsLevel(){
+    //  let filteredQuestionsByDifficulty = questions.filter(question => question.difficulty === selectLevel);
+    //  selectLevel = filteredQuestionsByDifficulty;
+
+    // Set Question counter
+     questionsCounter.innerHTML = "Question " + (questionsCounterIndex + 1) + " of " +
+     quiz.length;
+
+    let randomQuestions = Math.floor(Math.random() * quiz.length );
+    currentQuestion = quiz[randomQuestions];
+
+    quiz.splice(randomQuestions, 1);
+
+    questionsText.innerHTML = currentQuestion.question
+
+    shuffledQuestions = quiz.sort(() => Math.random() - .5);
+
+    for(let i = 0; i < currentQuestion.options.length; i++){
+        optionBtn[i].innerHTML = currentQuestion.options[i];
+        optionBtn.addEventListener("click", function() {
+        
+     });
+        
     }
 
-    // Set Questions Counter and display random Questions
-    function selectNewQuestions(){
-        // Set Question counter
-        questionsCounter.innerHTML = "Question " + (questionsCounterIndex + 1) + " of " +
-        quiz.length;
-        // Set Question text
-        const questionsIndex = availableSelections[Math.floor(Math.random() * availableSelections.length)];
-        currentQuestion = questionsIndex;
-        questionsText.innerHTML = currentQuestion.question;
-        console.log(questionsIndex);
-
-        // Index number of questions in the availableSelections Array
-        const indexNum = availableSelections.indexOf(questionsIndex);
-
-        // Remove the questionsIndex from the availableSelections Array, so the question is not repeated
-        availableSelections.splice(indexNum, 1);
-
-        // Set the options, loop through options Array and push it into the availabeOptions Array 
-        const optionLen = currentQuestion.options.length;
-        for(let i = 0; i < optionLen; i++){
-            availableOptions.push(i);
+     if (selectLevel == 'level1') {
+        shuffledQuestions = questionsLevel1;
+    } else if (selectLevel == "level2") {
+        shuffledQuestions= questionsLevel2;
+    } else if (selectLevel == "level3") {
+        shuffledQuestions= questionsLevel3;
+    }   else {
+        shuffledQuestions = randomQuestions;
         }
-        // Set random Options and Append options container to the available options
-        // First loop through the options
-        for(let i = 0; i < optionLen; i++){
-            const randOption = availableOptions[Math.floor(Math.random() * availableOptions.length)];
-            const optionIndex = availableOptions.indexOf(randOption);
-        // Remove the OptionIndex from the availableOptions Array, so the option is not repeated
-            availableOptions.splice(optionIndex, 1);
-            const option = document.getElementsByClassName("option-btn");
-            option.innerHTML = currentQuestion.options[randOption];
-            // option = currentQuestion.options[i];
-            option = i;
-            optionsBtn.appendChild(option);
-        }
+
+        setNextQuestions();
+    };
+
+    function difficultyL1Questions() {
+        quiz = questionsLevel1;
+        displayQuestionsLevel();
+
+
+    };
+
+    function difficultyL2Questions(){};
+
+    function difficultyL3Questions(){};
     
-        questionsCounterIndex++;
-        console.log(indexNum);
-    }
+    function setNextQuestions(){
+        displayQuestionsLevel(shuffledQuestions[currentQuestion]);
+    };
 
-    // const nextQ = document.getElementById("next-ques-button")
 
-    // nextQ.addEventListener("click", function() {
-    //     if (questionsCounter === quiz.length){
-    //         console.log("finish quiz");
+    // nextQueBtn.addEventListener("click", function() { 
+    //             }
+    //         });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Function that loops through questions then Push questions into the selectedQuestions Array
+    // function availableQuestions() {
+    //     // const totalQuestions = quiz.length
+    //     for(let i = 0; i< quiz.length; i++) {
+    //         selectedQuestions.push(quiz[i]) 
     //     }
-    //     else{
-    //         selectNewQuestions();  
-    //     }
-    // });
+    //     console.log(selectedQuestions);
+    // }
 
-        // function nextQ(){
-        //     if (questionsCounter === quiz.length){
-        //         console.log("finish quiz");
-        //     }
-        //     else{
-        //         selectNewQuestions();  
-        //     }
+    // // Set Questions Counter and display random Questions
+    // function selectRandomQuestions(){
+    //     // Set Question counter
+    //     questionsCounter.innerHTML = "Question " + (questionsCounterIndex + 1) + " of " +
+    //     quiz.length;
+    //     // Set Question text
+    //     const randomQuestions = selectedQuestions[Math.floor(Math.random() * selectedQuestions.length)];
+    //     currentQuestion = randomQuestions;
+    //     questionsText.innerHTML = currentQuestion.questions-txt;
+    //     console.log(randomQuestions);
+    // }
 
-        // }
 
-    //  window.onload = fuction() {
-    // // //    Set all questions in  selectedQuestions Array
-    //     availableSelections();
 
-    // // Call the selectNewQuestions function's function
-    //     selectNewQuestions();
 
-    //  };
+
+
+
+
+
+
+
+
+//     // Function that loops through questions then Push questions into the selectedQuestions Array
+//     function availableQuestions() {
+//         // const totalQuestions = quiz.length
+//         for(let i = 0; i< quiz.length; i++) {
+//             selectedQuestions.push(quiz[i]) 
+//         }
+//         console.log(selectedQuestions);
+//     }
+
+//     // Set Questions Counter and display random Questions
+//     function selectRandomQuestions(){
+//         // Set Question counter
+//         questionsCounter.innerHTML = "Question " + (questionsCounterIndex + 1) + " of " +
+//         quiz.length;
+//         // Set Question text
+//         const questionsIndex = availableQuestions[Math.floor(Math.random() * availableQuestions.length)];
+//         currentQuestion = questionsIndex;
+//         questionsText.innerHTML = currentQuestion.questions-txt;
+//         console.log(questionsIndex);
+
+//         // Index number of questions in the availableSelections Array
+//         const indexNum = availableQuestions.indexOf(questionsIndex);
+
+//         // Remove the questionsIndex from the availableSelections Array, so the question is not repeated
+//         availableQuestions.splice(indexNum, 1);
+
+//         // Set the options, loop through options Array and push it into the availabeOptions Array 
+//         const optionLen = currentQuestion.options.length;
+//         for(let i = 0; i < optionLen; i++){
+//             availableOptions.push(i);
+//         }
+//         // Set random Options and Append options container to the available options
+//         // First loop through the options
+//         for(let i = 0; i < optionLen; i++){
+//             const randOption = availableOptions[Math.floor(Math.random() * availableOptions.length)];
+//             const optionIndex = availableOptions.indexOf(randOption);
+//         // Remove the OptionIndex from the availableOptions Array, so the option is not repeated
+//             availableOptions.splice(optionIndex, 1);
+//             const option = document.getElementsByClassName("option-btn");
+//             option.innerHTML = currentQuestion.options[randOption];
+//             // option = currentQuestion.options[i];
+//             option = i;
+//             option.appendChild(option);
+//         }
+    
+//         questionsCounterIndex++;
+//         console.log(indexNum);
+//     }
+
+//     const nextQuestionBtn = document.getElementById("next-ques-button");
+
+//     nextQueBtn.addEventListener("click", function() {
+//         if (questionsCounter === quiz.length){
+//             console.log("finish quiz");
+//         }
+//         else{
+//             selectNewQuestions();  
+//         }
+//     });
+
+
+//       window.onload = ()=> {
+//     //     Set all questions in  selectedQuestions Array
+//          availableQuestions();
+
+//     // // Call the selectNewQuestions function's function
+//          selectNewQuestions();
+
+//      };
 
 //     // Function to randomly display quiz with level of diffivulty
 //     function difficultyL1() {
@@ -230,18 +326,16 @@ if (errorTxt !== '') {
 
 // // let filteredQuestionsByDifficulty = questions.filter(question => question.difficulty === selectedLevel);
 
-//         }
+        
 //     });
+// }
 
-
-
-
-// function showResults(){};
-// function runQuiz() {
-//     startQuiz.classList.add('hide');
-//     ruleBox.classList.add.remove('hide');
-//     selection.classList.remove('hide')
-// };
- // homeBtn.onclick = ()=>{
-    //     window.location.reload(); //reload the current window
-    // }
+// // function showResults(){};
+// // function runQuiz() {
+// //     startQuiz.classList.add('hide');
+// //     ruleBox.classList.add.remove('hide');
+// //     selection.classList.remove('hide')
+// // };
+//  // homeBtn.onclick = ()=>{
+//     //     window.location.reload(); //reload the current window
+    // };
